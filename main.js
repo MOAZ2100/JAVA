2ed44e1df4bdf499d2aa2164c2391096b6d3384b
MOAZ2100 <moazmohamedforma9@gmail.com>
2025-06-09 00:54:42 +0300

    if

---
+++ main.js
@@ -0,0 +1,14 @@
+let degree = +prompt("Enter your degree ")
+if(100 < degree){
+    console.log("You are enter Wrong degree")
+}else if(93 <= degree ){
+    console.log("Your grade is Exllent")
+}else if(83 <= degree ){
+    console.log("Your grade is Very good")
+}else if(70 <= degree ){
+    console.log("Your grade is Good")
+}else if(60 <= degree ){
+    console.log("Your grade is Acceptable")
+}else{
+    console.log("Your grade is Fall")
+}
